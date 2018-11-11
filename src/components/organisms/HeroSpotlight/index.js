e5831cf49af03dbc35b836ef3194549c588d057c
Mateus Santana <mateuss.dev@gmail.com>
2018-11-11 01:49:19 -0200

    Add HeroSpotlight

---
+++ src/components/organisms/HeroSpotlight/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+import { Row, HeroTitle, HeroDescription } from 'atoms'
+import { Button } from 'molecules'
+
+const HeroSpotlightStyled = styled(Row)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 500px;
+  margin: auto auto;
+  text-align: center;
+`
+
+const HeroSpotlight = props => (
+  <HeroSpotlightStyled>
+    <HeroTitle>{props.title}</HeroTitle>
+    <HeroDescription>{props.description}</HeroDescription>
+    <Button secondary>Find your holliday partner</Button>
+  </HeroSpotlightStyled>
+)
+
+HeroSpotlight.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+}
+
+export default HeroSpotlight
