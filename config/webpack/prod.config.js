480cacbe79cff1056d85dbe554074ccb1731bc35
Mateus Santana <mateuss.dev@gmail.com>
2018-11-11 23:18:36 -0200

    Adjusts build

--- config/webpack/prod.config.js
+++ config/webpack/prod.config.js
@@ -2,7 +2,6 @@ const webpack = require('webpack')
 const common = require('./common')
 
 const HtmlPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const CleanPlugin = require('clean-webpack-plugin')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const Copy = require('copy-webpack-plugin')
@@ -14,37 +13,13 @@ module.exports = {
 
   plugins: [
     new CleanPlugin(['build'], { root: common.paths.root }),
-    new ExtractTextPlugin({ filename: '[name]-[hash].css' }),
     new webpack.DefinePlugin({ 'process.env': { NODE_ENV: '"production"' } }),
-
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'react-build',
-      chunks: ['main'],
-      minChunks: ({ resource }) => /node_modules\/(react(-dom)?|fbjs)\//.test(resource),
-    }),
-
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      chunks: ['main'],
-      minChunks: ({ resource }) => /node_modules/.test(resource),
-    }),
-
     new HtmlPlugin(
       Object.assign({}, common.htmlPluginConfig, {
         minify: { collapseWhitespace: true },
-
-        chunksSortMode: (chunk1, chunk2) => {
-          const order = ['react-build', 'vendor', 'main']
-          const left = order.indexOf(chunk1.names[0])
-          const right = order.indexOf(chunk2.names[0])
-          return left - right
-        },
       }),
     ),
     new Copy(common.copyLoader),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-    }),
   ].concat(process.env.ANALYZER ? new BundleAnalyzerPlugin() : []),
 
   module: {
