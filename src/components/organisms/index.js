b95f28bb92bc473a9513bf154b72f705e8cf7b30
Mateus Santana <mateuss.dev@gmail.com>
2018-11-13 18:27:15 -0200

    Add SearchPartners on index organisms

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -3,5 +3,13 @@ import Hero from './Hero'
 import HeroHeader from './HeroHeader'
 import HowDreamshare from './HowDreamshare'
 import Footer from './Footer'
+import SearchPartners from './SearchPartners'
 
-export { HeroSpotlight, Hero, HeroHeader, HowDreamshare, Footer }
+export {
+  HeroSpotlight,
+  Hero,
+  HeroHeader,
+  HowDreamshare,
+  Footer,
+  SearchPartners,
+}
