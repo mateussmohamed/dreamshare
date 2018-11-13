dd46b718f35b40a103b9d00d322f908bc2acfd09
Mateus Santana <mateuss.dev@gmail.com>
2018-11-13 10:53:01 -0200

    Remove unnecessary code

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Layout } from 'templates'
 import { Home } from 'pages'
 
-// import components from 'components'
-// import { Layout, Home } from 'components'
-// console.log(components)
-
 const App = () => {
   return (
     <Layout>
