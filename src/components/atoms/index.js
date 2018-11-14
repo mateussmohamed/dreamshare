a912122c0f467d15c8156535712b97cddeb44553
Mateus Santana <mateuss.dev@gmail.com>
2018-11-14 16:30:47 -0200

    Add new atoms

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -3,7 +3,19 @@ import Container from './Container'
 import HeroDescription from './HeroDescription'
 import HeroImage from './HeroImage'
 import HeroTitle from './HeroTitle'
+import HeroContainer from './HeroContainer'
+import HeroContent from './HeroContent'
 import Logo from './Logo'
 import Row from './Row'
 
-export { Column, Container, HeroDescription, HeroImage, HeroTitle, Logo, Row }
+export {
+  Column,
+  Container,
+  HeroDescription,
+  HeroImage,
+  HeroTitle,
+  HeroContainer,
+  HeroContent,
+  Logo,
+  Row,
+}
